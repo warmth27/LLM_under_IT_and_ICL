28271591315d738ca162427491ed14fa997a3f7f
warmth27 <144099301+warmth27@users.noreply.github.com>
2024-09-16 10:40:44 +0800

    Add files via upload

---
+++ LLM-main/InContext Leaning(ICL)/Gemma2/train.py
@@ -0,0 +1,122 @@
+#! model = gemma-2-9b-it
+#! ICL
+
+import json
+import pandas as pd
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description="Run experiments with different seeds and num_class values.")
+parser.add_argument('--seed', type=int, required=True, help='Random seed')
+parser.add_argument('--num_class', type=int, required=True, help='Number of examples per class')
+args = parser.parse_args()
+
+def dataset_jsonl_transfer(origin_path, new_path):
+    examples = []
+    with open(origin_path, "r") as file:
+        for line in file:
+            data = json.loads(line)
+            context = data["text"]
+            label = data["output"]
+            example = {
+                "input": f"tweet: {context}\n",
+                "output": f"label: {label} \n",
+            }
+            examples.append(example)
+
+    with open(new_path, "w", encoding="utf-8") as file:
+        for example in examples:
+            file.write(json.dumps(example, ensure_ascii=False) + "\n")
+
+def generate_prompt(examples):
+    prompt = ""
+    for example in examples:
+        prompt += example['input'] + example['output']
+    return prompt
+
+def predict(prompt, model, tokenizer, new_input):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    temperature = 0.2
+
+    prompt += "Analyze the content and determine if it includes {label}.Respond only with {label} or not {label}, without providing any additional context or explanation." 
+
+    new_input = f"{new_input}"        
+
+    
+    messages = [
+         {"role": "user", "content": f"{prompt}\n{new_input}"}
+    ]
+    
+
+
+    text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+    generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=50, temperature = temperature)
+
+    generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    return response
+
+
+model_path = "/home/pod/gemma2-9b-it"
+train_dataset_path = "/home/pod/llm_gemma2/data_process/bragging/train_pro.jsonl"
+test_dataset_path = "/home/pod/llm_gemma2/data_process/bragging/test_pro_nolabel.jsonl"
+train_jsonl_new_path = "/home/pod/llm_gemma2/data_process/bragging/new_train_pro.jsonl"
+test_jsonl_new_path = "/home/pod/llm_gemma2/data_process/bragging/new_test_pro_nolabel.jsonl"
+
+if not os.path.exists(train_jsonl_new_path):
+    dataset_jsonl_transfer(train_dataset_path, train_jsonl_new_path)
+if not os.path.exists(test_jsonl_new_path):
+    dataset_jsonl_transfer(test_dataset_path, test_jsonl_new_path)
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
+
+
+with open(train_jsonl_new_path, "r") as file:
+    train_examples = [json.loads(line) for line in file]
+
+
+random_seed = args.seed
+random.seed(random_seed)
+torch.manual_seed(random_seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(random_seed)
+
+bragging_examples = [ex for ex in train_examples if "label: bragging" in ex['output']]
+not_bragging_examples = [ex for ex in train_examples if 'not bragging' in ex['output']]
+
+num_class = args.num_class
+selected_examples = random.sample(bragging_examples, num_class) + random.sample(not_bragging_examples, num_class)
+
+
+with open(f"/home/pod/llm_gemma2/bragging/sample/gemma2_bragging_icl_{num_class}_{random_seed}.txt", "w", encoding="utf-8") as f:
+    for example in selected_examples:
+        f.write(example['input'] + example['output'] + "\n")
+
+
+test_df = pd.read_json(test_jsonl_new_path, lines=True)
+results = []
+
+
+for index, row in test_df.iterrows():
+    new_input = row['input']
+    
+    prompt = generate_prompt(selected_examples) 
+
+    response = predict(prompt, model, tokenizer, new_input)
+
+   
+    predictions = {
+        "input": row["input"],
+        "prediction": response,
+    }
+    results.append(predictions)
+
+with open(f"/home/pod/llm_gemma2/bragging/results/gemma2_bragging_icl_{num_class}_{random_seed}.json", "w", encoding='utf-8') as f:
+    json.dump(results, f, ensure_ascii=False, indent=4)
---
+++ LLM-main/Instruction tuning(IT)/MetaLlama/llama3_train.py
@@ -0,0 +1,202 @@
+#! model = meta-llama3-8b-instruct
+#! Lora ftuning
+
+import json
+import pandas as pd
+import torch
+from datasets import Dataset
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq, AutoTokenizer
+from transformers import LlamaTokenizer, LlamaForCausalLM
+import os
+import random
+import argparse
+
+
+
+parser = argparse.ArgumentParser(description="Run experiments with different seeds and num_class values.")
+parser.add_argument('--seed', type=int, required=True, help='Random seed')
+parser.add_argument('--num_class', type=int, required=True, help='Number of examples per class')
+args = parser.parse_args()
+
+
+random_seed = args.seed
+random.seed(random_seed)
+torch.manual_seed(random_seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(random_seed)
+
+
+def dataset_jsonl_transfer(origin_path, new_path):
+    examples = []
+    with open(origin_path, "r") as file:
+        for line in file:
+            data = json.loads(line)
+            context = data["text"]
+            label = data["output"]
+            example = {
+                "input": f"tweet: {context}\n",
+                "output": f"label: {label} \n",
+            }
+            examples.append(example)
+
+    with open(new_path, "w", encoding="utf-8") as file:
+        for example in examples:
+            file.write(json.dumps(example, ensure_ascii=False) + "\n")
+
+            
+
+def process_func(example):    
+
+    MAX_LENGTH = 512 
+    input_ids, attention_mask, labels = [], [], []
+    instruction = tokenizer(f"<|start_header_id|>system<|end_header_id|>\n Analyze the content and determine if it includes {label}.Respond only with {label} or not {label}, without providing any additional context or explanation.<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n{example['input']}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>\n",
+        add_special_tokens=False,
+    )
+    response = tokenizer(f"{example['output']}<|eot_id|>", add_special_tokens=False)
+    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
+    attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
+    if len(input_ids) > MAX_LENGTH:  
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {
+        "input_ids": input_ids,"attention_mask": attention_mask,"labels": labels
+    } 
+
+
+
+def predict(messages, model, tokenizer):
+    device = "cuda"
+    temperature = 0.2
+    
+    text = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+    generated_ids = model.generate(model_inputs.input_ids,max_new_tokens=80, temperature = temperature)
+
+    generated_ids = [
+        output_ids[len(input_ids) :]
+        for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    return response
+
+model_path = '/home/pod/shared-public/models/meta-llama/Meta-Llama-3-8B-Instruct'
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = LlamaForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
+model.enable_input_require_grads()  
+
+train_dataset_path = "/home/pod/llm_gemma2/data_process/complaint/train_pro.json"
+test_dataset_path = "/home/pod/llm_gemma2/data_process/complaint/test_pro_nolabel.json"
+train_jsonl_new_path = "/home/pod/llm_gemma2/data_process/complaint/new_train_pro.json"
+test_jsonl_new_path = "/home/pod/llm_gemma2/data_process/complaint/new_test_pro_nolabel.json"
+
+if not os.path.exists(train_jsonl_new_path):
+    dataset_jsonl_transfer(train_dataset_path, train_jsonl_new_path)
+if not os.path.exists(test_jsonl_new_path):
+    dataset_jsonl_transfer(test_dataset_path, test_jsonl_new_path)
+
+
+with open(train_jsonl_new_path, "r") as file:
+    train_examples = [json.loads(line) for line in file]
+
+num_class = args.num_class
+complaint_examples = [ex for ex in train_examples if "label: complaint" in ex['output']]
+not_complaint_examples = [ex for ex in train_examples if 'not complaint' in ex['output']]
+selected_examples = random.sample(complaint_examples, num_class) + random.sample(not_complaint_examples, num_class)
+
+
+with open(f"/home/pod/shared-nvme/lama/IT/complaint_results/sample/lama_complaint_{num_class}_{random_seed}.txt", "w", encoding="utf-8") as f:
+    for example in selected_examples:
+        f.write(example['input'] + example['output'] + "\n")
+
+
+
+selected_df = pd.DataFrame(selected_examples)
+selected_ds = Dataset.from_pandas(selected_df)
+train_dataset = selected_ds.map(process_func, remove_columns=selected_ds.column_names)
+
+
+
+config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    inference_mode=False,  
+    r=8,  
+    lora_alpha=32,  
+    lora_dropout=0.1,  
+)
+
+model = get_peft_model(model, config)
+
+
+args = TrainingArguments(
+    output_dir=f"/home/pod/shared-nvme/lama/IT/complaint_results/output/lama-{num_class}-{random_seed}",
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=8,
+    logging_steps=10,
+    num_train_epochs=3,
+    save_steps=2000,
+    learning_rate=1e-4,
+    save_on_each_node=True,
+    gradient_checkpointing=True,
+    report_to="none",
+)
+
+
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True)
+)
+
+trainer.train()
+
+
+test_df = pd.read_json(test_jsonl_new_path, lines=True)
+
+instruction = "Analyze the content and determine if it includes {label}.Respond only with {label} or not {label}, without providing any additional context or explanation."
+
+print("=================start to predict================")
+
+results = []
+
+test_text_list = []
+for index, row in test_df.iterrows():
+    instruction = instruction
+    input_value = row['input']
+    
+    messages = [
+        {"role": "system", "content": f"{instruction}"},
+        {"role": "user", "content": f"{input_value}"}
+    ]
+
+    response = predict(messages, model, tokenizer)
+
+    
+    prediction = {
+        "input": row["input"],
+        "prediction": response,
+    }
+    results.append(prediction)
+
+with open(f"/home/pod/shared-nvme/lama/IT/complaint_results/results/lama_complaint_{num_class}_{random_seed}.json", "w", encoding='utf-8') as f:
+    json.dump(results, f, ensure_ascii=False, indent=4)
