face5e1cd3937e2034abe9109d21636781a0263d
warmth27 <144099301+warmth27@users.noreply.github.com>
2024-09-16 10:41:13 +0800

    Delete LLM-main directory

--- LLM-main/InContext Leaning(ICL)/run.sh
+++
@@ -1,7 +0,0 @@
-
-for num_class in 1 8 16 32; do
-    for seed in 42 43 44 45 46; do
-        echo "Running with num_class=$num_class and seed=$seed"
-        python qwn2_7b_train.py --seed $seed --num_class $num_class
-    done
-done
